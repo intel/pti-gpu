0f481bed64b9fd6308fb58784f1e6f23006ed67c
Matthew Schilling <matthew.schilling@intel.com>
2025-05-07 13:51:45 -0400

    PTI-112 - Comply with trademark violations noted by Q/A (#551)
    (cherry picked from commit 4de8724629b37da448dde67671b5a88a884d6e0c)
    
    Co-authored-by: tomasrodi <tomas.rodriguez@intel.com>

--- sdk/include/pti/pti_view.h
+++ sdk/include/pti/pti_view.h
@@ -396,7 +396,7 @@ PTI_EXPORT const char*
 ptiViewMemcpyTypeToString( pti_view_memcpy_type type );
 
 /**
- * @brief Returns current pti host timestamp in nanoseconds. The timestamp is in the same domain as view records timestamps.
+ * @brief Returns current Intel(R) PTI host timestamp in nanoseconds. The timestamp is in the same domain as view records timestamps.
  *
  * @return uint64_t
  */
@@ -412,7 +412,7 @@ ptiViewGetTimestamp();
 typedef uint64_t (*pti_fptr_get_timestamp)( void );
 
 /**
- * @brief Sets callback to user provided timestamping function.  This will replace the default pti host timestamper.
+ * @brief Sets callback to user provided timestamping function.  This will replace the default Intel(R) PTI host timestamper.
  *        Multiple callbacks that set differing timestamp function, through the session; will result in differing
  *        timestamp domains in the view record buffer.
  *
