intel/pti-gpu

commit 9062623a29d26497f9b6c0a47b688f27dd8fcc06

[PTI] Add Readable Version String to Binaries (#882)
Add human readable version string, including git commit hash, to binary
(pti_view/pti).

Signed-off-by: Schilling, Matthew <matthew.schilling@intel.com>

sdk/cmake/Modules/macros.cmake
@@ -584,3 +584,23 @@ macro(AddVersionlessLinkFile MY_TARGET)
     $<TARGET_LINKER_FILE_DIR:${MY_TARGET}>/${CMAKE_SHARED_LIBRARY_PREFIX}${MY_TARGET}$<$<PLATFORM_ID:Windows>:$<$<CONFIG:Debug>:d>>${CMAKE_LINK_LIBRARY_SUFFIX}
   )
 endmacro()
+
+function(GetCurrentGitCommit COMMIT_OUTPUT_VAR)
+  set(_OUT_TEMP "")
+  set(_GIT_RESULT_TEMP 1)
+  if (NOT Git_FOUND)
+    find_package(Git)
+  endif()
+  if(Git_FOUND)
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" rev-parse HEAD
+      OUTPUT_VARIABLE _OUT_TEMP
+      RESULT_VARIABLE _GIT_RESULT_TEMP
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if(_GIT_RESULT_TEMP EQUAL 0)
+      set(${COMMIT_OUTPUT_VAR} "${_OUT_TEMP}" PARENT_SCOPE)
+    endif()
+  endif()
+endfunction()

sdk/src/pti_version.cc
@@ -5,8 +5,23 @@
 // =============================================================
 #include "pti/pti_version.h"
 
+#include "platform_config.h"
+
+#if defined(_MSC_VER) || !defined(__clang__)
+#pragma comment(linker, "/include:kPtiIdentifierString")
+#endif
+
+extern "C" {
+PTI_USED extern const char kPtiIdentifierString[] =  // NOLINT
+    "Intel(R) Profiling Tools Interfaces (PTI) Version " PTI_VERSION_STRING
+    " (" PTI_COMMIT_HASH_STRING ")";
+}  // extern "C"
+
+namespace {
 constexpr auto kPtiVersion = pti_version{PTI_VERSION_MAJOR, PTI_VERSION_MINOR, PTI_VERSION_PATCH};
+
 constexpr const char* const kPtiVersionString = PTI_VERSION_STRING;
+}  // namespace
 
 const char* ptiVersionString() { return kPtiVersionString; }
 

sdk/CMakeLists.txt
@@ -23,6 +23,15 @@ ProjectIsTopLevel()
 SetBuildType()
 
 # Global Settings
+
+if(PROJECT_IS_TOP_LEVEL AND NOT DEFINED PTI_COMMIT_HASH)
+  GetCurrentGitCommit(PTI_COMMIT_HASH)
+endif()
+
+if(NOT DEFINED PTI_COMMIT_HASH OR PTI_COMMIT_HASH STREQUAL "")
+  set(PTI_COMMIT_HASH "unknown")
+endif()
+
 if(PROJECT_IS_TOP_LEVEL)
   set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin CACHE STRING "")
   set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib CACHE STRING "")

sdk/include/pti/pti_version.h.in
@@ -24,6 +24,8 @@ extern "C" {
 #define PTI_VERSION_MINOR @PROJECT_VERSION_MINOR@
 #define PTI_VERSION_PATCH @PROJECT_VERSION_PATCH@
 
+#define PTI_COMMIT_HASH_STRING "@PTI_COMMIT_HASH@"
+
 typedef struct pti_version {
   uint32_t _major;
   uint32_t _minor;

sdk/src/utils/platform_config.h.in
@@ -7,7 +7,20 @@
 #ifndef UTILS_PLATFORM_CONFIG_H_
 #define UTILS_PLATFORM_CONFIG_H_
 
+#if defined(__GNUC__) || defined(__clang__)
+#ifdef __has_attribute
+#if __has_attribute(__used__)
+#define PTI_USED __attribute__((__used__))
+#endif
+#else
+#define PTI_USED __attribute__((__used__))
+#endif
+#endif
+
+#ifndef PTI_USED
+#define PTI_USED
+#endif
+
 #cmakedefine PTI_EXPERIMENTAL_FILESYSTEM
 
 #endif  // UTILS_PLATFORM_CONFIG_H_
-

commit 6577c5a860623b60ce844d17899e36880abcdf97

[PTI] Fix g++ compiler build (#888)
* Fix g++/gcc build failure caused by 9062623a29d26497f9b6c0a47b688f27dd8fcc06 .
* Fix L0 cmake patch with Windows line endings.
* Adjust CMake to use CMake Find* modules

Signed-off-by: Schilling, Matthew <matthew.schilling@intel.com>

sdk/.gitignore
@@ -8,4 +8,12 @@ venv/
 compile_commands.json
 CMakeUserPresets.json
 error_diff.txt
+
+# Shortened Windows build directory
 wbr
+
+# Common test install directory
+out/
+
+# Raw install/packaging intermediary data
+drop_raw/

sdk/cmake/Modules/macros.cmake
@@ -220,6 +220,10 @@ macro(GetLevelZero PTI_L0_LOADER PTI_L0_LOADER_COMMIT_HASH)
       set(LZ_BASE_DIR ${FETCHCONTENT_BASE_DIR})
     endif()
 
+    if (NOT Git_FOUND)
+      find_package(Git REQUIRED)
+    endif()
+
     include(FetchContent)
     FetchContent_Declare(
         LevelZero
@@ -227,8 +231,8 @@ macro(GetLevelZero PTI_L0_LOADER PTI_L0_LOADER_COMMIT_HASH)
         https://github.com/oneapi-src/level-zero.git
         GIT_TAG ${PTI_L0_LOADER_COMMIT_HASH}
         # Patch: Fix VERSION file location to avoid Windows <version> header conflict (CMAKE_BINARY_DIR -> CMAKE_CURRENT_BINARY_DIR)
-        PATCH_COMMAND git checkout .
-        COMMAND git apply --ignore-whitespace "${PROJECT_SOURCE_DIR}/cmake/levelzero_version_fix.patch"
+        PATCH_COMMAND "${GIT_EXECUTABLE}" checkout .
+        COMMAND "${GIT_EXECUTABLE}" apply --ignore-whitespace --whitespace=fix "${PROJECT_SOURCE_DIR}/cmake/levelzero_version_fix.patch"
     )
     # Prevent content from automatically being installed with PTI
     FetchContent_GetProperties(LevelZero)

sdk/cmake/levelzero_version_fix.patch
@@ -3,9 +3,9 @@
 @@ -74,7 +74,7 @@ add_definitions(-DLOADER_VERSION_MAJOR=${PROJECT_VERSION_MAJOR})
  add_definitions(-DLOADER_VERSION_MINOR=${PROJECT_VERSION_MINOR})
  add_definitions(-DLOADER_VERSION_PATCH=${PROJECT_VERSION_PATCH})
-
+ 
 -file(WRITE "${CMAKE_BINARY_DIR}/VERSION" "${PROJECT_VERSION}")
 +file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/VERSION" "${PROJECT_VERSION}")
-
+ 
  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)

sdk/src/pti_version.cc
@@ -7,7 +7,7 @@
 
 #include "platform_config.h"
 
-#if defined(_MSC_VER) || !defined(__clang__)
+#if defined(_MSC_VER) && !defined(__clang__)
 #pragma comment(linker, "/include:kPtiIdentifierString")
 #endif
 